50c9869922fbc1ef4870e9ce73f3a0253135ddeb
Daniel <deumolo@gmail.com>
2023-06-10 10:32:41 -0500

    Added item management functions to cart slice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,5 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../cartItems';
 const initialState = {
   cartItems: [...cartItems],
   amount: 4,
@@ -8,18 +8,36 @@ const initialState = {
 };
 
 const cartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState,
   reducers: {
     clearCart: (state) => {
       state.cartItems = [];
       state.amount = 0;
     },
+    deleteItem: (state, { payload }) => {
+      state.cartItems = state.cartItems.filter((item) => item.id !== payload);
+    },
+    incrementItem: (state, { payload }) => {
+      console.log(payload);
+      const item = state.cartItems.find((item) => item.id === payload);
+      item.amount++;
+    },
+    decrementItem: (state, { payload }) => {
+      console.log(payload);
+      const item = state.cartItems.find((item) => item.id === payload);
+      if (item.amount === 1) {
+        state.cartItems = state.cartItems.filter((item) => item.id !== payload);
+      } else {
+        item.amount--;
+      }
+    },
   },
 });
 
 // console.log(cartSlice);
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, deleteItem, incrementItem, decrementItem } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
