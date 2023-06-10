7eb6ccb403d64c943c3612db276025a28045fb03
Daniel Moreno <deumolo@gmail.com>
2023-06-10 00:16:25 -0600

    Created initial setup for CartItem component

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,25 @@
-const CartItem = (props) => {
+import { ChevronUp, ChevronDown } from "../icons";
+
+const CartItem = ({ id, img, title, price, amount }) => {
   return (
     <div>
-      <p>{props.title} </p>
+      <article className="cart-item">
+        <img src={img} alt={title} />
+        <div>
+          <h4>{title}</h4>
+          <h4 className="item-price">$ {price}</h4>
+          <div className="remove-btn">remove</div>
+        </div>
+        <div>
+          <button className="amount-btn">
+            <ChevronUp />
+          </button>
+          <p className="amount">{amount}</p>
+          <button className="amount-btn">
+            <ChevronDown />
+          </button>
+        </div>
+      </article>
     </div>
   );
 };
