5d2bddddd3549671b30a1d2348abd0f5e574307b
Daniel Moreno <deumolo@gmail.com>
2023-06-10 00:02:09 -0600

    Created initial setup for CartContainer component and CartItem component

---
+++ src/components/CartItem.js
@@ -0,0 +1,9 @@
+const CartItem = (props) => {
+  return (
+    <div>
+      <p>{props.title} </p>
+    </div>
+  );
+};
+
+export default CartItem;
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,14 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit';
-
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
 const initialState = {
-  cartItems: [],
-  amount: 0,
+  cartItems: [...cartItems],
+  amount: 4,
   total: 0,
   isLoading: true,
 };
 
 const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState,
 });
 
