15465fb6c3646382e66b1f24081218d142c606a3
Daniel <deumolo@gmail.com>
2023-06-11 14:56:58 -0500

    Added modal functionality to cart container component

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,11 +1,11 @@
-import CartItem from "./CartItem";
-import { useSelector, useDispatch } from "react-redux";
-import { clearCart } from "../features/cart/cartSlice";
+import CartItem from './CartItem';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggle } from '../features/modal/modalSlice';
 
 const CartContainer = () => {
   const dispatch = useDispatch();
-
   const { cartItems, total, amount } = useSelector((store) => store.cart);
+
   if (amount < 1) {
     return (
       <section className="cart">
@@ -31,13 +31,13 @@ const CartContainer = () => {
         <hr />
         <div className="cart-total">
           <h4>
-            total <span>${total}</span>
+            total <span>${total.toFixed(2)}</span>
           </h4>
         </div>
         <button
           className="btn clear-btn"
           onClick={() => {
-            dispatch(clearCart());
+            dispatch(toggle());
           }}
         >
           CLEAR CART
